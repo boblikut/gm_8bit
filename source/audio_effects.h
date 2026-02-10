c2541ec48fb6b45b799b9485bcb96cd4870121ba
boblikut <125297488+boblikut@users.noreply.github.com>
2026-02-10 23:17:18 +0300

    Update audio_effects.h

--- source/audio_effects.h
+++ source/audio_effects.h
@@ -7,7 +7,8 @@ namespace AudioEffects {
 	enum {
 		EFF_NONE,
 		EFF_BITCRUSH,
-		EFF_DESAMPLE
+		EFF_DESAMPLE,
+		EFF_GASMASK
 	};
 
 	void BitCrush(uint16_t* sampleBuffer, int samples, float quant, float gainFactor) {
@@ -20,6 +21,19 @@ namespace AudioEffects {
 			sampleBuffer[i] *= quant;
 			sampleBuffer[i] *= gainFactor;
 		}
+	}
+
+	void GasMask(uint16_t* sampleBuffer, int samples, float muffled) {
+	    float cutoff = 0.02f + (1.0f - muffled) * 0.08f;
+	    float gain = 1.0f - muffled * 0.3f;
+	    float prev = 0.0f;
+	    for (int i = 0; i < samples; i++) {
+	        float sample = (float)sampleBuffer[i];
+	        sample = cutoff * sample + (1.0f - cutoff) * prev;
+	        prev = sample;
+	        sample *= gain;
+	        sampleBuffer[i] = (uint16_t)sample;
+	    }
 	}
 
 	static uint16_t tempBuf[10 * 1024];
