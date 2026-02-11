a0d01309e7cf0baf29519eb2be4e05773348a0dc
boblikut <125297488+boblikut@users.noreply.github.com>
2026-02-11 20:57:06 +0300

    Update eightbit_state.h

--- source/eightbit_state.h
+++ source/eightbit_state.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <string>
 #include <unordered_map>
+#include "audio_effects.h"
+
+struct Effect {
+	AudioEffect eff_id;
+	std::vector<float> eff_args;
+};
 
 struct EightbitState {
 	int crushFactor = 350;
@@ -9,5 +15,5 @@ struct EightbitState {
 	int desampleRate = 2;
 	uint16_t port = 4000;
 	std::string ip = "127.0.0.1";
-	std::unordered_map<int, std::tuple<IVoiceCodec*, int>> afflictedPlayers;
+	std::unordered_map<int, std::tuple<IVoiceCodec*, std::vector<Effect>>> afflictedPlayers;
 };
