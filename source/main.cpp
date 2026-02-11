8313e92a2d5c1f68305892c9268c90788ed89821
boblikut <125297488+boblikut@users.noreply.github.com>
2026-02-11 23:02:03 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -101,8 +101,8 @@ void hook_BroadcastVoiceData(IClient* cl, uint nBytes, char* data, int64 xuid) {
 		#endif
 
 		//Apply audio effect
-		std::vector<Effects> effs = std::get<1>(afflicted_players.at(uid));
-		std::map<Effects, function<uint16_t*, int, std::vector<float>>> eff_funcs = g_eightbit->effects_functions;
+		std::vector<Effect> effs = std::get<1>(afflicted_players.at(uid));
+		std::map<AudioEffects, function<uint16_t*, int, std::vector<float>>> eff_funcs = g_eightbit->effects_functions;
 		for (int i = 0; i < effs.size - 1; i++){
 			Effect eff = effs.at(i);
 			eff_funcs[eff.eff_id]((uint16_t*)&decompressedBuffer, samples, eff.eff_args);
