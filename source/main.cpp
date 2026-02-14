62d49b54c535af1d3056c099fad2360f0708bb38
boblikut <125297488+boblikut@users.noreply.github.com>
2026-02-15 01:25:53 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -2,7 +2,6 @@
 
 #include <GarrysMod/Lua/Interface.h>
 #include <GarrysMod/FactoryLoader.hpp>
-#include <GarrysMod/InterfacePointers.hpp>
 #include <scanning/symbolfinder.hpp>
 #include <detouring/hook.hpp>
 #include <iostream>
