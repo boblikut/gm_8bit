6d310268dce538a78a1147f06d00023c9d9383f4
boblikut <125297488+boblikut@users.noreply.github.com>
2026-02-15 12:35:13 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -136,11 +136,7 @@ void hook_BroadcastVoiceData(IClient* cl, uint nBytes, char* data, int64 xuid) {
 		
 		for(int i=0; i < sv->GetClientCount(); i++)
 		{
-			IClient *pDestClient = sv->GetClient(i);
-	
-			
-	
-			pDestClient->SendNetMsg( voiceData );
+			Msg(i);
 		}
 	}
 	else {
