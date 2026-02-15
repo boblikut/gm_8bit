ee778a1c0a27cc177379793f69ad5b3ce9f7cb4d
boblikut <125297488+boblikut@users.noreply.github.com>
2026-02-15 14:25:20 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -137,38 +137,10 @@ void hook_BroadcastVoiceData(IClient* cl, uint nBytes, char* data, int64 xuid) {
 
 		if (sv == nullptr){
 			Msg("sv = nullptr");
+			return detour_BroadcastVoiceData.GetTrampoline<SV_BroadcastVoiceData>()(cl, nBytes, data, xuid);
 		}
 		
-		for(int i=0; i < sv->GetClientCount(); i++)
-		{
-			IClient *pDestClient = sv->GetClient(i);
-	
-			bool bSelf = (pDestClient == cl);
-	
-			// Only send voice to active clients
-			if( !pDestClient->IsActive() )
-				continue;
-	
-			// Does the game code want cl sending to this client?
-	
-			bool bHearsPlayer = pDestClient->IsHearingClient( voiceData.m_nFromClient );
-			voiceData.m_bProximity = pDestClient->IsProximityHearingClient( voiceData.m_nFromClient );
-				
-			if ( !bHearsPlayer && !bSelf )
-				continue;	
-	
-			voiceData.m_nLength = nBytes * 8;
-	
-			// Is loopback enabled?
-			if( !bHearsPlayer )
-			{
-				// Still send something, just zero length (this is so the client 
-				// can display something that shows knows the server knows it's talking).
-				voiceData.m_nLength = 0;	
-			}
-	
-			pDestClient->SendNetMsg( voiceData );
-		}
+		
 	}
 	else {
 		return detour_BroadcastVoiceData.GetTrampoline<SV_BroadcastVoiceData>()(cl, nBytes, data, xuid);
