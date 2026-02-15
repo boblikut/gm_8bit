947dfc8604c9d380d4bc0624d0f6bb4fc551824d
boblikut <125297488+boblikut@users.noreply.github.com>
2026-02-15 22:52:35 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -290,14 +290,14 @@ LUA_FUNCTION_STATIC(eightbit_enableEffect) {
 		}
 		return 0;
 	}
-	else if(effs.at(0).eff_id == AudioEffects::EFF_NONE || effs.size() == 0) {
+	else if(effs.at(0).eff_id != AudioEffects::EFF_NONE && effs.size() > 0) {
 		IVoiceCodec* codec = new SteamOpus::Opus_FrameDecoder();
 		codec->Init(5, 24000);
 		if (top == 2){
 			afflicted_players.emplace(id, std::make_tuple(codec, std::vector<Effect>{}, effs, special_players));
 		} 
 		else {
-			//afflicted_players.emplace(id, std::make_tuple(codec, effs, std::vector<Effect>{}, std::unordered_set<std::string>{}));
+			afflicted_players.emplace(id, std::make_tuple(codec, effs, std::vector<Effect>{}, std::unordered_set<std::string>{}));
 		}	
 	}
 	return 0;
