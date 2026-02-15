580b508f8837ee83cd04a7f2a046f440d6ad4ec0
boblikut <125297488+boblikut@users.noreply.github.com>
2026-02-15 14:05:43 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -133,11 +133,6 @@ void hook_BroadcastVoiceData(IClient* cl, uint nBytes, char* data, int64 xuid) {
 		voiceData.m_nLength = nBytes * 8;	// length in bits
 		voiceData.m_DataOut = data;
 		voiceData.m_xuid = xuid;
-
-		if (sv == nullptr) {
-			Msg("sv = nullptr");
-			return detour_BroadcastVoiceData.GetTrampoline<SV_BroadcastVoiceData>()(cl, nBytes, data, xuid);
-		}
 		
 		for(int i=0; i < sv->GetClientCount(); i++)
 		{
